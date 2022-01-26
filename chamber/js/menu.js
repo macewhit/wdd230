b5aaed0c1a58119077055cb7ac32b0e6d72612b8
macewhit <macymwhitney@gmail.com>
2022-01-26 00:12:35 -0700

    init chamber home

---
+++ chamber/js/menu.js
@@ -0,0 +1,20 @@
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation');
+const exitbutton = document.querySelector('#xbutton');
+const ham = document.querySelector('#ham');
+
+menubutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+    exitbutton.classList.toggle('hidden');
+    ham.classList.toggle('hidden');
+}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) {
+        mainnav.classList.remove('responsive');
+        exitbutton.classList.add("hidden");
+        ham.classList.remove("hidden");
+    }
+};
+

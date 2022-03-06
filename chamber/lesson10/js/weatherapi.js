eacf571e57cfbf88a545e96fdab4a54dcdc26c5f
macewhit <macymwhitney@gmail.com>
2022-03-06 00:40:18 -0700

    weather and css formatting home

---
+++ chamber/lesson10/js/weatherapi.js
@@ -0,0 +1,17 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8ce342ec0bef6453a2de8960f69f0afa'
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
+
+

c440cc5e0300a4ae4978172b88cc140f065e694c
macewhit <macymwhitney@gmail.com>
2022-04-04 21:59:20 -0600

    temple page update: -temples html -reservation html -thank you html -css

--- temple/js/spotlight.js
+++ temple/js/spotlight.js
@@ -9,18 +9,18 @@ fetch(requestURL)
         const temples = jsonObject['temples'];
         const temple = temples[Math.floor(Math.random() * temples.length)];
 
-        //img
+        //img Temple photo
         let image = document.createElement('img');
         image.src = temple.imgfile;
         image.setAttribute('alt', temple.name);
         spotlight.appendChild(image);
 
-        //h4
+        //h4 Name
         let head = document.createElement('h4');
         head.textContent = temple.name;
         spotlight.appendChild(head);
 
-        //p phone
+        //p Summary
         let par = document.createElement('p');
         par.textContent = temple.summary;
         spotlight.appendChild(par);

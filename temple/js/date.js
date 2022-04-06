d89e29976aa92310ef77081659caeaafa6bda02f
macewhit <macymwhitney@gmail.com>
2022-04-05 21:24:38 -0600

    suites page and last modified in footer

--- temple/js/date.js
+++ temple/js/date.js
@@ -4,6 +4,7 @@ const year = d.getFullYear();
 
 // using getElementById
 document.querySelector("#currentyear").textContent = year;
+document.getElementById("lastupdated").textContent = `Last Updated: ${document.lastModified}`;
 
 // element to output to
 const datefieldUK = document.querySelector(".date");
